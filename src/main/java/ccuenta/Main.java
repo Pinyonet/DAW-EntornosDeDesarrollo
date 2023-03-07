3d6020e4b2448312669487472db15933965fa8f6
Pinyonet <46427657+Pinyonet@users.noreply.github.com>
2023-03-07 19:38:18 +0100

    Inicial

--- src/main/java/ccuenta/Main.java
+++ src/main/java/ccuenta/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     public static void main(String[] args) {
         CCuenta cuenta1;
-        float cantidad = 250;
+        float cantidad = 20000000;
 
         cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
         operativa_cuenta(cuenta1, cantidad);
